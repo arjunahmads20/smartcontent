b9c173d014a14b066ba3ceae214a5a97368f0af9
arjunahmads20 <arjunahmads20@gmail.com>
2026-07-15 10:41:23 +0700

    Update release build configuration for ProGuard

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,9 +30,11 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
-            minifyEnabled = true
-            useProguard = true 
-            proguardFiles = getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'       
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), 
+                "proguard-rules.pro"
+            }
         }
     }
 }
